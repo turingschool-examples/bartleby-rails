f67e71842c52ad5e689980c86b0816b477d255d9
Steve Kinney <hello@stevekinney.net>
2014-11-27 16:00:59 -0700

    Set up some basic tests

--- test/controllers/notes_controller_test.rb
+++ test/controllers/notes_controller_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class NotesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "responds to json" do
+    get :index, { format: :json }
+    assert_response :success
+  end
+
+  test "index records exist under note root" do
+    get :index, { format: :json }
+    response_body = JSON.parse(response.body)
+    assert response_body["note"]
+    assert_equal 1, response_body.keys.count
+  end
+
+  test "index returns an array of records" do
+    get :index, { format: :json }
+    response_body = JSON.parse(response.body)
+    assert_kind_of Array, response_body["note"]
+  end
 end
